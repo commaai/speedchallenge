85396303d29741de61b9215ad81e37c9a79d986f
jaketerrito <jaketerrito@github.com>
2019-02-21 09:36:38 -0800

    JAKE

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import cv2
 from generator import data_generator
-from tensorflow.keras.layers import Input, Dense, Flatten, Conv3D, MaxPooling3D, MaxPooling2D
+from tensorflow.keras.layers import Input, Dense, Flatten, Conv3D, MaxPooling3D, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, RMSprop
 
-batch_size = 128
-sequence_length = 3
+batch_size = 32
+sequence_length = 2
 epochs = 200
 
 cap = cv2.VideoCapture('./data/train.mp4')
@@ -29,20 +29,20 @@ gen = data_generator(cap, video_size, batch_size, sequence_length, (width,height
 inputs = Input((sequence_length,height,width,3))
 
 # A convolution being applied to each image seperately
-x = Conv3D(32,(1,5,5),strides=(1,3,3),activation='relu')(inputs) 
+x = Conv3D(32,(1,3,3),strides=(1,2,2),activation='relu')(inputs)
 x = Conv3D(32,(1,3,3),strides=(1,2,2),activation='relu')(x)
-x = Conv3D(32,(1,2,2),strides=(1,2,2),activation='relu')(x)
+x = Conv3D(32,(1,3,3),strides=(1,2,2),activation='relu')(x) 
+x = BatchNormalization()(x)
 
 # A convolution across all images together
-x = Conv3D(32,(sequence_length,5,5),strides=(1,2,2),activation='relu')(x)
-
-x = MaxPooling3D((1,2,2))(x) #Pooling this convolution
+x = Conv3D(16,(sequence_length,1,1),strides=(1,1,1),activation='relu')(x)
 x = Flatten()(x) 
-x = Dense(128,activation='relu')(x)
+
 x = Dense(32,activation='relu')(x)
+x = Dense(16,activation='relu')(x)
 outputs = Dense(1,activation='relu')(x)
 model = Model(inputs=inputs,outputs=outputs)
-model.compile(SGD(0.01,momentum=0.9),loss='mean_squared_error')
+model.compile(RMSprop(),loss='mean_squared_error')
 
 print(model.summary())
 
